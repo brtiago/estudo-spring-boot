c82fd0d2c01958c0722f2eef6409a8d0a0c4ccde
Tiago <727320+brtiago@users.noreply.github.com>
2023-08-31 05:23:46 -0300

    Feat: criação de DTO com o registro DadosCadastroMedico, DadosEndereco

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,12 +1,13 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
     @PostMapping
-    public void cadastrar(@RequestBody String jason) {
-        System.out.println(jason);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
     }
 }
